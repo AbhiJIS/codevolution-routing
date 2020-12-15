19f347a32fd52f9ead336339683373b9178709d7
Abhishek Kumar Singh <abhishek.singh.789@outlook.com>
2020-12-15 07:53:57 +0530

    added relative route

--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -1,47 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
   template: `
     <h3>
-      department list
+      Department List
     </h3>
     <ul class="items">
       <li *ngFor="let department of departments" [class.selected]="isSelected(department)" (click)="onSelect(department)">
         <span class="badge">{{department.id}}</span> {{department.name}}
       </li>
-    </ul>
+  </ul>
   `,
   styles: []
 })
 export class DepartmentListComponent implements OnInit {
 
-  public selectedID;
-
+  public selectedId;
   departments = [
-    {id: '1', name: 'Angular'},
-    {id: '2', name: 'Node.js'},
-    {id: '3', name: 'MongoDB'},
-    {id: '4', name: 'Python'},
-    {id: '5', name: 'Bootstrap'}
-  ];
-
+    {id: 1, name: 'Angular'},
+    {id: 2, name: 'Node'},
+    {id: 3, name: 'MongoDB'},
+    {id: 4, name: 'Ruby'},
+    {id: 5, name: 'Bootstrap'}
+  ]
   constructor(private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       let id = parseInt(params.get('id'));
-      this.selectedID = id;
-    });
+      this.selectedId = id;
+
+    } );
   }
 
-  onSelect(department){
-    this.router.navigate(['/departments', department.id], { relativeTo: this.route });
+  onSelect(department) {
+    //this.router.navigate(['/departments', department.id]);
+     this.router.navigate([department.id], { relativeTo: this.route });
   }
 
-  isSelected(department){
-    return department.id === this.selectedID;
-  }
-
+  isSelected(department) { return department.id === this.selectedId; }
 }
